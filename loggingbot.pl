d00cb4b4082e65a4840e92f794d8e1a0c9f644c6
910JQK <v910JQK@gmail.com>
2013-12-14 20:08:35 +0800

    131214.1

---
+++ loggingbot.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/perl
+
+use warnings;
+#use utf8;
+#use Text::Unidecode;
+#use Bot::BasicBot;
+#use WWW::Mechanize;
+
+
+package bot;
+use base 'Bot::BasicBot';
+#use Encode;
+
+$logdir=""
+
+sub connected {
+  $ENV{TZ}='UTC';
+  my $self = shift;
+  print STDERR $self->nick." connected\n";
+#  $self->join('#linuxbar');
+  $self->say(channel => '#linuxbar', body => 'Hello, I am '.$self->nick);
+  open(F,">>IRClog")
+}
+
+sub said {
+  $datestring = localtime();
+  my $self = shift;
+  my $message = shift;
+  my $channel = $message->{channel};
+  my $who = $message->{who};
+  my $rawnick = $message->{raw_nick};
+  my $body = $message->{body};
+  print F "[SAID][$datestring][$channel] $rawnick/$who:$body";
+}
+
+sub chanjoin {
+  $datestring = localtime();
+  my $self = shift;
+  my $message = shift;
+  my $channel_which = $message->{channel};
+  my $whojoin = $message->{who};
+  print F "[INFO][$datestring] $whojoin Joined $channel_which";
+}
+
+sub userquit {
+  $datestring = localtime();
+  my $self = shift;
+  my $message = shift;
+  my $whoquit = $message->{who};
+  my $body_what = $message->{body};
+  print F "[INFO][$datestring] $whoquit Quit with message $body_what";
+}
+
+package main;
+
+bot->new( 
+channels => ["#wecase" , "#linuxbar" , "#linuxba" , "##Orz" , "#ubuntu-cn" , "#archlinux-cn" , "#c_lang_cn"] ,
+server => "irc.freenode.net" ,
+port => "6667",
+nick => "JQKbot",
+alt_nicks => ["JQKbot1", "JQKbot2"],
+username  => "L_logging-test",
+name      => "A logging bot belongs to JQK.Generate log only for himself when he is offline."
+)->run();
+
